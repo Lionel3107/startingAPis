07b628eda3938519c017946b1df221ec0f185504
lionel3107 <dlmsonelterror@gmail.com>
2024-06-14 11:05:05 +0000

    intialized app and installed express

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+const students = [
+    {
+    id:1, 
+    firstName:"Lionel", 
+    lastName:"Kabore",
+    age:20,
+    },
+    {
+    id:2, 
+    firstName:"Farel", 
+    lastName:"Kabore",
+    age:17,
+
+    },
+    {
+    id:3, 
+    firstName:"Christian", 
+    lastName:"Kabore",
+    age:9,
+
+    },
+];
