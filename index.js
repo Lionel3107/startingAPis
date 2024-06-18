86f4cc079bc5b6d8a7f36cf41b99941cf7224b5d
lionel3107 <dlmsonelterror@gmail.com>
2024-06-18 11:26:56 +0000

    CRUD added

--- index.js
+++ index.js
@@ -24,11 +24,71 @@ const students = [
 
     },
 ];
-
+// Middleware
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json("Welcome to my API");
+    res.json("Welcome to API made by CS25");
 });
+
+app.get('/api/students',(req, res) => {
+    const student = {
+        
+            id: students.length + 1, 
+            firstName: req.body.firstName, 
+            lastName: req.body.lastName,
+            age: req.body.age,
+        
+    };
+
+    students.push(student);
+
+    res.status(201).json(students);
+});
+
+
+
+
+app.get("/api/students/:studentId", (req, res) => {
+    const student = students.find((s)=>s.id === parseInt(req.params.studentId));
+
+    if(!student){
+        res.status(404).json({message:"Student not found"})
+    } else {
+        res.status(200).json(student);
+    }
+
+
+});
+
+// Update a student
+app.put("/api/students/:studentId", (req, res) =>{
+    const student = students.find((s) => s.id === parseInt(req.params.studentId));
+    if (!student)  return res.status(404).json({message : "student not found"});
+
+    student.firstName = req.body.firstName;
+    student.lastNametName = req.body.lastName;
+    student.age = req.body.age;
+    res.status(200).json(student);
+
+});
+
+
+// Delete a student by ID
+app.delete("/api/students/:studentId", (req, res) =>{
+    const studentIndex = students.findIndex(
+        (s) => s.id === parseInt(req.params.studentId)
+    ); 
+
+    if(studentIndex === -1)
+        return res.status(404).json({message: "Student not found"});
+
+
+        const deletedStudent = students.splice(studentIndex, 1);
+        res.status(200).json(deletedStudent);    
+});
+
+
 
 const port = 8000
 
